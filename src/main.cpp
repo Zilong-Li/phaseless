08fec386a9f69ad937d1e319b4859431cfbb0a68
Zilong-Li <zimusen94@gmail.com>
2023-08-08 17:19:41 +0200

    add stuff

---
+++ src/main.cpp
@@ -0,0 +1,304 @@
+/*******************************************************************************
+ * @file        https://github.com/Zilong-Li/phaseless/src/main.cpp
+ * @author      Zilong Li
+ * Copyright (C) 2023. The use of this code is governed by the LICENSE file.
+ ******************************************************************************/
+#define _DECLARE_TOOLBOX_HERE
+
+#include "admixture.hpp"
+#include "fastphase.hpp"
+#include "phaseless.hpp"
+#include "utils.hpp"
+#include <argparse/argparse.hpp>
+
+using namespace argparse;
+
+int main(int argc, char * argv[])
+{
+    // ========= helper message and parameters parsing ===========================
+
+    const std::string VERSION{"0.2.5"};
+
+    // clang-format off
+    ArgumentParser program("phaseless", VERSION, default_arguments::version);
+    program.add_argument("-D","--debug")
+        .help("enable debug mode")
+        .default_value(false)
+        .implicit_value(true);
+
+
+    argparse::ArgumentParser cmd_impute("impute", VERSION, default_arguments::help);
+    cmd_impute.add_description("run imputation for low coverage sequencing data");
+    cmd_impute.add_argument("-c", "--cluster")
+        .help("number of ancestral haplotype clusters")
+        .default_value(10)
+        .scan<'i', int>();
+    cmd_impute.add_argument("-C", "--collapse")
+        .help("collapse SNPs in a reasonable window")
+        .default_value(false)
+        .implicit_value(true);
+    cmd_impute.add_argument("-B", "--grid-size")
+        .help("number of SNPs (>1) in each grid. 1 disables collapsing")
+        .default_value(1)
+        .scan<'i', int>();
+    cmd_impute.add_argument("-f", "--vcf")
+        .help("vcf/bcf format with GL/PL tag as input")
+        .default_value(std::string{""});
+    cmd_impute.add_argument("-g", "--beagle")
+        .help("gziped beagle format as input")
+        .default_value(std::string{""});
+    cmd_impute.add_argument("-i", "--iterations")
+        .help("number of EM iterations")
+        .default_value(40)
+        .scan<'i', int>();
+    cmd_impute.add_argument("-n", "--threads")
+        .help("number of threads")
+        .default_value(1)
+        .scan<'i', int>();
+    cmd_impute.add_argument("-o", "--out")
+        .help("output prefix")
+        .default_value(std::string{"impute"});
+    cmd_impute.add_argument("-p", "--no-print")
+        .help("disable print log to screen")
+        .default_value(false)
+        .implicit_value(true);
+    cmd_impute.add_argument("-r", "--region")
+        .help("region in vcf/bcf to subset")
+        .default_value(std::string{""});
+    cmd_impute.add_argument("-s", "--chunksize")
+        .help("size of each chunk in sites unit ")
+        .default_value(10000)
+        .scan<'i', int>();
+    cmd_impute.add_argument("-S", "--single-chunk")
+        .help("treat input as big single chunk")
+        .default_value(false)
+        .implicit_value(true);
+    cmd_impute.add_argument("--seed")
+        .help("seed for reproducing results")
+        .default_value(999)
+        .scan<'i', int>();
+    cmd_impute.add_argument("--minRecombRate")
+        .help("min recombination rate to determine if a SNP should be collapsed")
+        .default_value(1e-4)
+        .scan<'g', double>();
+    cmd_impute.add_parents(program);
+
+    argparse::ArgumentParser cmd_admix("admix", VERSION, default_arguments::help);
+    cmd_admix.add_description("run admixture with cluster likelihoods as input");
+    cmd_admix.add_argument("-a", "--no-accel")
+        .help("disable accelerated EM")
+        .default_value(false)
+        .implicit_value(true);
+    cmd_admix.add_argument("-b", "--bin")
+        .help("binary format from impute command as input")
+        .default_value(std::string{""});
+    cmd_admix.add_argument("-k", "--ancestry")
+        .help("number of ancestry in admixture assumption")
+        .default_value(2)
+        .scan<'i', int>();
+    cmd_admix.add_argument("-i", "--iterations")
+        .help("number of maximun EM iterations")
+        .default_value(1000)
+        .scan<'i', int>();
+    cmd_admix.add_argument("-n", "--threads")
+        .help("number of threads")
+        .default_value(4)
+        .scan<'i', int>();
+    cmd_admix.add_argument("-o", "--out")
+        .help("output prefix")
+        .default_value(std::string{"admix"});
+    cmd_admix.add_argument("-p", "--no-print")
+        .help("disable print log to screen")
+        .default_value(false)
+        .implicit_value(true);
+    cmd_admix.add_argument("-q", "--qtol")
+        .help("tolerance of stopping criteria for diff(Q)")
+        .default_value(1e-6)
+        .scan<'g', double>();
+    cmd_admix.add_argument("-l", "--ltol")
+        .help("tolerance of stopping criteria for diff(loglikelihood)")
+        .default_value(1e-1)
+        .scan<'g', double>();
+    cmd_admix.add_argument("-s","--seed")
+        .help("seed for reproducing results")
+        .default_value(999)
+        .scan<'i', int>();
+    cmd_admix.add_parents(program);
+
+    argparse::ArgumentParser cmd_convert("convert", VERSION, default_arguments::help);
+    cmd_convert.add_description("different file format converter");
+    cmd_convert.add_argument("-i","--input")
+        .help("input file to be converted")
+        .default_value(std::string{""});
+    cmd_convert.add_argument("-o","--output")
+        .help("output file prefix")
+        .default_value(std::string{"convert"});
+    cmd_convert.add_argument("-p", "--plink2beagle")
+        .help("use plink1 file as input without .bed")
+        .default_value(false)
+        .implicit_value(true);
+    cmd_convert.add_argument("-n", "--threads")
+        .help("number of threads")
+        .default_value(4)
+        .scan<'i', int>();
+    cmd_convert.add_argument("-s", "--chunksize")
+        .help("size of each chunk in sites unit ")
+        .default_value(10000)
+        .scan<'i', int>();
+    cmd_convert.add_parents(program);
+
+    argparse::ArgumentParser cmd_parse("parse", VERSION, default_arguments::help);
+    cmd_parse.add_description("manipulate pars.bin file outputted by impute command");
+    cmd_parse.add_argument("-b", "--bin")
+        .help("binary format from impute command as input")
+        .default_value(std::string{""});
+    cmd_parse.add_argument("-c", "--chunk")
+        .help("which chunk to extract (0-based), negative means all chunks")
+        .default_value(0)
+        .scan<'i', int>();
+    cmd_parse.add_argument("-s", "--samples")
+        .help("extract samples in the file, one sample id per line")
+        .default_value(std::string{""});
+    cmd_parse.add_argument("-o", "--out")
+        .help("output prefix")
+        .default_value(std::string{"parse"});
+    cmd_parse.add_parents(program);
+
+    argparse::ArgumentParser cmd_joint("joint", VERSION, default_arguments::help);
+    cmd_joint.add_argument("-c", "--cluster")
+        .help("number of haplotype clusters")
+        .default_value(10)
+        .scan<'i', int>();
+    cmd_joint.add_argument("-k", "--ancestry")
+        .help("number of ancestry")
+        .default_value(3)
+        .scan<'i', int>();
+    cmd_joint.add_argument("-g", "--beagle")
+        .help("gziped beagle format as input")
+        .default_value(std::string{""});
+    cmd_joint.add_argument("-i", "--iterations")
+        .help("number of EM iterations")
+        .default_value(40)
+        .scan<'i', int>();
+    cmd_joint.add_argument("-n", "--threads")
+        .help("number of threads")
+        .default_value(1)
+        .scan<'i', int>();
+    cmd_joint.add_argument("-o", "--out")
+        .help("output prefix")
+        .default_value(std::string{"joint"});
+    cmd_joint.add_argument("-p", "--no-print")
+        .help("disable print log to screen")
+        .default_value(false)
+        .implicit_value(true);
+    cmd_joint.add_argument("-S", "--single-chunk")
+        .help("treat input as big single chunk")
+        .default_value(false)
+        .implicit_value(true);
+    cmd_joint.add_argument("--seed")
+        .help("seed for reproducing results")
+        .default_value(999)
+        .scan<'i', int>();
+    cmd_joint.add_parents(program);
+
+    program.add_subparser(cmd_impute);
+    program.add_subparser(cmd_admix);
+    program.add_subparser(cmd_parse);
+    program.add_subparser(cmd_convert);
+    program.add_subparser(cmd_joint);
+    // clang-format on
+
+    try
+    {
+        Options opts;
+        for(int i = 0; i < argc; i++) opts.opts_in_effect += " " + std::string{argv[i]};
+        program.parse_args(argc, argv);
+
+        if(program.is_subcommand_used(cmd_joint))
+        {
+            opts.in_beagle.assign(cmd_joint.get("--beagle"));
+            opts.out.assign(cmd_joint.get("--out"));
+            opts.C = cmd_joint.get<int>("--cluster");
+            opts.K = cmd_joint.get<int>("--ancestry");
+            opts.nthreads = cmd_joint.get<int>("--threads");
+            opts.nimpute = cmd_joint.get<int>("--iterations");
+            opts.seed = cmd_joint.get<int>("--seed");
+            opts.debug = cmd_joint.get<bool>("--debug");
+            opts.chunksize = INT_MAX;
+            if((opts.in_beagle.empty() && opts.in_vcf.empty()) || cmd_joint.get<bool>("--help"))
+                throw std::runtime_error(cmd_joint.help().str());
+            run_phaseless_main(opts);
+        }
+        else if(program.is_subcommand_used(cmd_impute))
+        {
+            opts.in_beagle.assign(cmd_impute.get("--beagle"));
+            opts.in_vcf.assign(cmd_impute.get("--vcf"));
+            opts.out.assign(cmd_impute.get("--out"));
+            opts.C = cmd_impute.get<int>("--cluster");
+            opts.gridsize = cmd_impute.get<int>("--grid-size");
+            opts.nthreads = cmd_impute.get<int>("--threads");
+            opts.nimpute = cmd_impute.get<int>("--iterations");
+            opts.seed = cmd_impute.get<int>("--seed");
+            opts.chunksize = cmd_impute.get<int>("--chunksize");
+            opts.single_chunk = cmd_impute.get<bool>("--single-chunk");
+            opts.collapse = cmd_impute.get<bool>("--collapse");
+            opts.noscreen = cmd_impute.get<bool>("--no-print");
+            opts.debug = cmd_impute.get<bool>("--debug");
+            opts.tol_r = cmd_impute.get<double>("--minRecombRate");
+            if(opts.single_chunk) opts.chunksize = INT_MAX;
+            if((opts.in_beagle.empty() && opts.in_vcf.empty()) || cmd_impute.get<bool>("--help"))
+                throw std::runtime_error(cmd_impute.help().str());
+            run_impute_main(opts);
+        }
+        else if(program.is_subcommand_used(cmd_admix))
+        {
+            opts.in_bin.assign(cmd_admix.get("--bin"));
+            opts.out.assign(cmd_admix.get("--out"));
+            opts.seed = cmd_admix.get<int>("--seed");
+            opts.K = cmd_admix.get<int>("-k");
+            opts.nthreads = cmd_admix.get<int>("--threads");
+            opts.nadmix = cmd_admix.get<int>("--iterations");
+            opts.qtol = cmd_admix.get<double>("--qtol");
+            opts.ltol = cmd_admix.get<double>("--ltol");
+            opts.noaccel = cmd_admix.get<bool>("--no-accel");
+            opts.noscreen = cmd_admix.get<bool>("--no-print");
+            opts.debug = cmd_admix.get<bool>("--debug");
+            if(opts.in_bin.empty() || cmd_admix.get<bool>("--help"))
+                throw std::runtime_error(cmd_admix.help().str());
+            run_admix_main(opts);
+        }
+        else if(program.is_subcommand_used(cmd_parse))
+        {
+            opts.in_bin.assign(cmd_parse.get("--bin"));
+            opts.out.assign(cmd_parse.get("--out"));
+            opts.samples = cmd_parse.get("--samples");
+            opts.ichunk = cmd_parse.get<int>("--chunk");
+            if(opts.in_bin.empty() || cmd_parse.get<bool>("--help"))
+                throw std::runtime_error(cmd_parse.help().str());
+            run_parse_main(opts);
+        }
+        else if(program.is_subcommand_used(cmd_convert))
+        {
+            opts.nthreads = cmd_convert.get<int>("--threads");
+            opts.in_plink = cmd_convert.get("--input");
+            opts.out = cmd_convert.get("--output");
+            opts.chunksize = cmd_convert.get<int>("--chunksize");
+            if(opts.in_plink.empty() || cmd_convert.get<bool>("--help"))
+                throw std::runtime_error(cmd_convert.help().str());
+            if(cmd_convert.get<bool>("--plink2beagle")) run_convert_main(opts);
+        }
+        else
+        {
+            cao.cerr("Contact author: Zilong Li (zilong.dk@gmail.com)\n");
+            cao.cerr(program.help().str());
+            std::exit(1);
+        }
+    }
+    catch(const std::exception & err)
+    {
+        std::cerr << err.what() << std::endl;
+        std::exit(1);
+    }
+
+    return 0;
+}
---
+++ src/phaseless.hpp
@@ -0,0 +1,91 @@
+#ifndef PHASELESS_H_
+#define PHASELESS_H_
+
+#include "common.hpp"
+#include <mutex>
+
+class Phaseless
+{
+  private:
+    std::mutex mutex_it; // in case of race condition
+
+  public:
+    Phaseless(int k, int c, int n, int m, int seed) : K(k), C(c), N(n), M(m), KK(k * k), CC(c * c)
+    {
+        auto rng = std::default_random_engine{};
+        rng.seed(seed);
+        P = RandomUniform<MyArr2D, std::default_random_engine>(M, C, rng, alleleEmitThreshold, 1 - alleleEmitThreshold);
+        Q = RandomUniform<MyArr2D, std::default_random_engine>(K, N, rng, admixtureThreshold, 1 - admixtureThreshold);
+        Q.rowwise() /= Q.colwise().sum(); // normalize Q per individual
+        F.resize(K);
+        for(int i = 0; i < K; i++)
+        {
+            F[i] = RandomUniform<MyArr2D, std::default_random_engine>(C, M, rng, clusterFreqThreshold,
+                                                                      1 - clusterFreqThreshold);
+            F[i].rowwise() /= F[i].colwise().sum();
+        }
+    }
+    ~Phaseless() {}
+
+    // BOUNDING
+    double minRate{0.1}, maxRate{100}; // threshold for R
+    double clusterFreqThreshold{1e-4}; // threshold for F
+    double alleleEmitThreshold{1e-4}; // threshold for P
+    double admixtureThreshold{1e-4}; // threshold for Q
+
+    // FLAGS
+    bool debug{0};
+
+    // SHARED VARIBALES
+    const int K, C, N, M, KK, CC; // CC = C x C, KK = K x K
+    double nGen;
+    Int1D pos_chunk; // store the start pos of each chunk in the full scale
+    Int1D dist; // physical position distance between two markers
+    MyArr1D er, et; // M, jumping rate
+    std::vector<MyArr2D> F; // K x C x M, ancestral cluster frequency
+    MyArr2D P; // M x C, ancestral cluster-specific allele frequence
+    MyArr2D Q; // K x N, admixture proportions for all individuals
+    MyArr2D GP; // N x (M x 3), genotype probabilies for all individuals
+    MyArr2D EclusterA1, EclusterA2; // C x M
+    MyArr2D Eancestry; // K x N
+    MyArr2D PclusterK; // C x K x M
+
+    void initRecombination(const Int1D & pos, double Ne, int B);
+    void initRecombination(const Int2D & pos, double Ne, int B);
+    void initIteration();
+    void updateIteration();
+    double runForwardBackwards(const int, const int, const MyFloat1D &, bool);
+    double runBigass(int, const MyFloat2D &, bool);
+    void getForwardPrevSums(const MyArr2D &, MyArr2D &, MyArr2D &, MyArr2D &, MyArr1D &);
+    void getBackwardPrevSums(const MyArr2D &, MyArr2D &, MyArr2D &, MyArr2D &, MyArr1D &, double &, int, int);
+    void moveForward(int,
+                     int,
+                     MyArr2D &,
+                     const MyArr1D &,
+                     const MyArr2D &,
+                     const MyArr2D &,
+                     const MyArr2D &,
+                     const MyArr2D &,
+                     const MyArr1D &);
+    void moveBackward(int,
+                      int,
+                      MyFloat,
+                      MyArr2D &,
+                      const MyArr2D &,
+                      const MyArr2D &,
+                      const MyArr2D &,
+                      const MyArr2D &,
+                      const MyArr1D &,
+                      const double);
+    void getPosterios(const int,
+                      const int,
+                      const MyArr2D &,
+                      const MyArr2D &,
+                      const MyArr1D &,
+                      const std::vector<MyArr2D> &,
+                      const std::vector<MyArr2D> &);
+};
+
+int run_phaseless_main(Options & opts);
+
+#endif // PHASELESS_H_
