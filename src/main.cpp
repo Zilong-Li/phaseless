c0bc88dae63dcb94ebf2b1290f1e79071d60490f
Zilong-Li <zimusen94@gmail.com>
2024-07-17 14:47:34 +0200

    versioning

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char * argv[])
 {
     // ========= helper message and parameters parsing ===========================
 
-    const std::string VERSION{"0.5.1"};
+    const std::string VERSION{"0.5.2"};
 
     // below for catching ctrl+c, and dumping files
     struct sigaction sa;
